74bf5e2fb0fa4ca1c710c30299df1c6574ceba41
3Sm0k1nG2 <37252090+3Sm0k1nG2@users.noreply.github.com>
2024-07-30 20:23:55 +0200

    Add `Mods3012.CW.MorePlayersMod`

---
+++ mods/Mods3012.CW.MorePlayersMod/Mods3012.CW.MorePlayersMod/Patches/SteamLobbyHandlerPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using Steamworks;
+
+namespace Mods3012.CW.MorePlayersMod.Patches
+{
+    [HarmonyPatch(typeof(SteamLobbyHandler))]
+    internal class SteamLobbyHandlerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SteamLobbyHandler.HostMatch))]
+        public static void HostMatch(
+            Action<ulong> _action, bool privateMatch,
+            ref CallResult<LobbyCreated_t> ___m_OnLobbyCreatedCallBack,
+            ref int ___m_MaxPlayers
+        // Uncomment if in prefix
+        //ref bool ___m_isHostingPrivate,
+        //ref bool ___m_Joined,
+        //ref Action<ulong> ___OnHostedAction
+        )
+        {
+            ___m_MaxPlayers = Plugin.ConfigEntries.LobbyMaxPlayersCount.Value;
+            Plugin.Logger.LogMessage($"Updated Max Players to {___m_MaxPlayers}.");
+
+            // Uncomment if in prefix
+            //___m_isHostingPrivate = privateMatch;
+            //___m_Joined = false;
+            //___OnHostedAction = _action;
+
+            SteamAPICall_t steamAPICall_t = SteamMatchmaking.CreateLobby(privateMatch ? ELobbyType.k_ELobbyTypeFriendsOnly : ELobbyType.k_ELobbyTypePrivate, ___m_MaxPlayers);
+            ___m_OnLobbyCreatedCallBack.Set(steamAPICall_t, null);
+
+            Plugin.Logger.LogInfo($"SteamLobbyHandler Hosting With Max Players: {___m_MaxPlayers}");
+        }
+    }
+}
---
+++ mods/Mods3012.CW.MorePlayersMod/Mods3012.CW.MorePlayersMod/PluginConfigEntries.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Configuration;
+
+namespace Mods3012.CW.MorePlayersMod
+{
+    internal class PluginConfigEntries
+    {
+        internal ConfigEntry<int> LobbyMaxPlayersCount { get; private set; }
+
+        internal PluginConfigEntries(ConfigFile configFile)
+        {
+            LobbyMaxPlayersCount = configFile.Bind(
+                "UserPreferences",
+                "PlayersCount",
+                16,
+                "The count of allowed players in a single lobby."
+            );
+        }
+    }
+}
